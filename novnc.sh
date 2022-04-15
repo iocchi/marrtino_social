e936a9ca01733a1267179dc8d66e261180c1a2e2
ferrarini09@gmail.com <ferrarini09@gmail.com>
2022-04-15 02:58:25 +0200

    Update novnc.sh

--- novnc.sh
+++ novnc.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 echo "Launch NOVNC"
-# sudo apt install -y novnc x11vnc 
+
+echo "installation -------------------"
+echo "sudo apt install -y novnc x11vnc"
+echo "--------------------------------" 
 # ------------------------------------
 x11vnc -display :0 -autoport -localhost -nopw -bg -xkb -ncache -ncache_cr -quiet -forever
-/usr/share/novnc/utils/launch.sh --listen 8081 --vnc localhost:5900
+/usr/share/novnc/utils/launch.sh --listen 8085 --vnc localhost:5900
