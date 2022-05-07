cdab7f68ee896a36b12cdad581c6ea294c58676a
Fabio Ferrarini <ferrarini09@gmail.com>
2022-05-07 09:03:46 +0200

    v.2.0

---
+++ install/inst_shellinabox.sh
@@ -0,0 +1,7 @@
+echo "https://www.unixmen.com/shellinabox-a-web-based-ajax-terminal-emulator/"
+sudo apt-get install shellinabox -y
+echo  "configuration"
+sudo pico  /etc/default/shellinabox
+echo "usage : https://ip-address-of-remote-servers:9200"
+
+
