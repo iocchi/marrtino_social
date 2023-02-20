0fcc8f81bef1092f600186dc717704c9c0e77caa
Fabio Ferrarini <ferrarini09@gmail.com>
2023-02-20 14:52:18 +0100

    Navigation Full Screen without joy

--- facestart.sh
+++ facestart.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh 
 # echo "Launch Browser"
 #DISPLAY=:0 midori -p -e Fullscreen -a http://localhost:8080/social/marrtina.html 
 chromium-browser --app=http://localhost:8080/social/marrtina.html   -start-maximized
